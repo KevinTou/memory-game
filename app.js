f53b0e30a018896d383bc8b55253c882ea39ede4
Kevin Tou <toukevin@gmail.com>
2017-11-08 14:19:33 -0800

    Created a basic grid (which needs to be re-worked) and included a "shuffle" implementation using Arrays for the basis of the game's logic

---
+++ app.js
@@ -0,0 +1,47 @@
+// var cell = $('<div class="grid"></div>');
+// var containerBox = $("#main-container");
+var originalArray = ["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8"];
+var tempArray = [];
+
+// function createGrid() {
+//     for (var row = 0; row < 4; row++) {
+//         for(var column = 0; column < 4; column++) {
+//             containerBox.append(cell);            
+//         }
+//     }
+// }
+
+// $(document).ready(function() {
+//     createGrid();
+// });
+
+// function isMatched() {
+
+// }
+
+// function isFlipped() {
+
+// }
+
+// function clearGrid() {
+
+// }
+
+function shuffle() {
+    for(var i = originalArray.length - 1; i >= 0; i--) {
+        var randomIndex = Math.floor(Math.random() * originalArray.length);
+        var newIndex = originalArray.splice(randomIndex, 1);
+        tempArray.push(newIndex);
+    }
+    for(var j = 0; j < 16; j++){
+        console.log("["+j+"] is " + tempArray[j]);
+    }
+}
+
+// function newGame() {
+    
+// }
+
+// function countMoves() {
+
+// }
