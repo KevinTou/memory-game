5e8a2cb62cf2fa21658f6297b8e883c340904b0d
Kevin Tou <toukevin@gmail.com>
2017-11-10 00:26:24 -0800

    Updated grid creation and fixed the shuffle function (previously added arrays instead of array elements) such that the shuffled elements are appended as classes to each div

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-// var cell = $('<div class="grid"></div>');
-// var containerBox = $("#main-container");
-var originalArray = ["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8"];
+var originalArray = ["one", "one", "two", "two", "three", "three", "four", "four", "five", "five", "six", "six", "seven", "seven", "eight", "eight"];
 var tempArray = [];
 
-// function createGrid() {
-//     for (var row = 0; row < 4; row++) {
-//         for (var column = 0; column < 4; column++) {
-//             containerBox.append(cell);            
-//         }
-//     }
-// }
-
-// $(document).ready(function() {
-//     createGrid();
-// });
+function createGrid() {
+    var containerBox = $("#main-container")
+    for (var box = 0; box < 16; box++) {
+        containerBox.append('<div class="grid"></div>');            
+    }
+}
 
 // function isMatched() {
 
@@ -31,8 +24,11 @@ function shuffle() {
     for (var i = originalArray.length - 1; i >= 0; i--) {
         var randomIndex = Math.floor(Math.random() * originalArray.length);
         var newIndex = originalArray.splice(randomIndex, 1);
-        tempArray.push(newIndex);
+        tempArray.push(newIndex[0]);
     }
+    $('div.grid').each(function (index) {
+        $(this).addClass(tempArray[index]);
+    });
 }
 
 function newGame() {
@@ -45,3 +41,8 @@ function newGame() {
 // function countMoves() {
 
 // }
+
+$(document).ready(function() {
+    createGrid();
+    shuffle();
+});
